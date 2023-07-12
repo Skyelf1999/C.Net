04bc0b134e4da52020852961e3543a6140a474ae
Di Shihao <109121873+Skyelf1999@users.noreply.github.com>
2023-07-12 17:20:17 +0800

    添加Mac上的c#代码

---
+++ C#_onMac/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OnMac
+{
+    class Program
+    {
+        /// <summary>
+        /// 使用yield实现可迭代访问对象
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<int> enumFunc()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 9;
+            yield break;
+            yield return 100;
+        }
+
+
+        static void Main(string[] args)
+        {
+            TestEnumerable test = new TestEnumerable();
+            foreach(int number in test) Console.WriteLine(number);
+
+        }
+    }
+
+}
